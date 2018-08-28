fc14beefae095486aca82ae15fc89c42c33cef46
Sarodus <jordiavellapol@gmail.com>
2018-08-28 20:46:26 +0200

    app stateless

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PxlsPlace from './components/PxlsPlace'
 import Controls from './components/Controls'
-import Picker from './components/Picker'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <PxlsPlace />
-        <Controls />
-      </div>
-    )
-  }
-}
+const App = () =>
+  <div>
+    <PxlsPlace />
+    <Controls />
+  </div>
 
 export default App
